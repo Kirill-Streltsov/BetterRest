5b6f034e3dd840106c6fa0c118342c9a0a149b13
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-07-28 17:53:55 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Kirill Streltsov on 28.07.23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
